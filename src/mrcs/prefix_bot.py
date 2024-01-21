ecac01947e98b7126b0d68d4d5f213eef9efc074
GEOEGII555 <57674559+GEOEGII555@users.noreply.github.com>
2024-01-21 21:19:56 +0300

    Update prefix_bot.py

--- src/mrcs/prefix_bot.py
+++ src/mrcs/prefix_bot.py
@@ -12,8 +12,8 @@ class PrefixBot(Client):
     prefix: str
     commands: dict[str]
 
-    def __init__(self, username: str, prefix: str, login_key: str = None) -> None:
-        super().__init__(username, login_key)
+    def __init__(self, username: str, prefix: str, login_key: str = None, bot_badge: bool = False) -> None:
+        super().__init__(username, login_key, bot_badge)
         if len(prefix) > 2048:
             raise ValueError("prefix is longer than 2048 characters")
         self.prefix = prefix
